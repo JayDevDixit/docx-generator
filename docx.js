149c95771a31ce3037496dab833209802c89f794
Jay Dev Dixit <jaydevdixit354@gmail.com>
2026-05-21 10:57:52 +0530

    image distortion issue resolved

--- docx.js
+++ docx.js
@@ -1,50 +1,90 @@
-import fs from 'fs/promises'
-import path from 'path'
-import { HeadingLevel, ImageRun, Packer, Paragraph, Document, AlignmentType, PageBreak } from 'docx'
-import { isImage, patch_dir } from './utility.js'
+import fs from "fs/promises";
+import path from "path";
+import {
+  HeadingLevel,
+  ImageRun,
+  Packer,
+  Paragraph,
+  Document,
+  AlignmentType,
+  PageBreak,
+  TextRun,
+} from "docx";
+import { isdir, isImage, MAX_WIDTH, patch_dir } from "./utility.js";
+import imageSize from "image-size";
 
-const createdocx = async (customer)=>{
-    let children = []
-    children = await attachFiles('pre',customer,children)
-    children = await attachFiles('post',customer,children)
-    const doc = new Document({sections: [{children}]})
-    const buffer = await Packer.toBuffer(doc)
-    const outputPath = path.join(patch_dir,day,`${customer}.docx`)
-    await fs.writeFile(outputPath,buffer)
-}
+const createdocx = async (customer) => {
+  let children = [];
+  children.push(new Paragraph({
+    alignment: AlignmentType.CENTER, spacing: {after: 600},children: [new TextRun({text: `Customer: ${customer}`,bold:true,color:'FF0000',size:32})]
+  }))
+  const preElements = await attachFiles("pre", customer);
+  const postElements = await attachFiles("post", customer);
+  preElements.length <= 0 ? console.log(`Fail to find Pre validation for ${customer}`) : ''
+  postElements.length <=0 ? console.log(`Fail to find Post validation for ${customer}`) : ''
+  children = [...children, ...preElements, ...postElements]
+  const doc = new Document({ sections: [{ children }] });
+  const buffer = await Packer.toBuffer(doc);
+  const outputPath = path.join(patch_dir, day, `${customer}.docx`);
+  await fs.writeFile(outputPath, buffer);
+};
 
-const attachFiles = async (state,customer,children) => {
-
-    const dir = path.join(patch_dir,day,customer,state)
-    const files = await fs.readdir(dir)
-    children.push(new Paragraph({text: `----------${state} Validation----------`,heading: HeadingLevel.HEADING_1,alignment:AlignmentType.CENTER}))
-    for(let file of files){
-        if(isImage(file)){
-        const imgPath = path.join(dir,file)
-        const image = await fs.readFile(imgPath)
-        children.push(new Paragraph({alignment:AlignmentType.CENTER,children:[
-            new ImageRun({data: image, transformation: {width: 600,height: 350}})
-        ]}))
-    } else {
-        console.log(`Ignoring non-image file ${file}`)
-    }
+const attachFiles = async (state, customer) => {
+const children = []
+  const dir = path.join(patch_dir, day, customer, state);
+  const files = await fs.readdir(dir);
+  children.push(
+    new Paragraph({
+      text: `----------${state.toUpperCase()} Validation----------`,
+      heading: HeadingLevel.HEADING_1,
+      alignment: AlignmentType.CENTER,
+      spacing: { after: 300 },
+    }),
+  );
+  for (let file of files) {
+    if (isImage(file)) {
+        const imgPath = path.join(dir, file);
+        const ext = path.extname(imgPath).toLocaleLowerCase().replace('.','')
         
+      const image = await fs.readFile(imgPath);
+      const dimensions = imageSize(image)
+      let width = dimensions.width
+      let height = dimensions.height
+      if(width > MAX_WIDTH){
+        const ratio = MAX_WIDTH / width;
+        width = Math.round(width*ratio)
+        height = Math.round(height*ratio)
+      }
+      children.push(
+        new Paragraph({
+          alignment: AlignmentType.CENTER,
+          spacing: { after: 300 },
+          children: [
+            new ImageRun({
+              data: image,
+              transformation: { width, height },
+              type: ext
+            }),
+          ],
+        }),
+      );
+    } else {
+      console.log(`Ignoring non-image file ${file}`);
     }
-    children.push(new Paragraph({children: [new PageBreak()]}))
+  }
+  children.push(new Paragraph({ children: [new PageBreak()] }));
 
-    return children
+  return children;
+};
+
+console.log("--------Creating docx file----------");
+const day = "4-May";
+const customers = await fs.readdir(path.join(patch_dir, day));
+
+const tasks = [];
+for (let customer of customers) {
+    await isdir(path.join(patch_dir, day,customer)) ? tasks.push(createdocx(customer)) : '';
 }
 
-
-console.log('--------Creating docx file----------')
-const day = '4-May'
-const customers = await fs.readdir(path.join(patch_dir,day))
-
-const tasks = []
-for(let customer of customers){
-    tasks.push(createdocx(customer))
-}
-
-await Promise.all(tasks)
-console.log('---------docx file created successfully----------')
-
+await Promise.all(tasks);
+console.log("---------docx file created successfully----------");
--- utility.js
+++ utility.js
@@ -1,5 +1,5 @@
 import path from "path";
-
+import fs from 'fs/promises'
 export const getDay = () =>{
     const date = new Date()
 
@@ -16,4 +16,11 @@ export const IMAGE_EXTENSIONS = ['.png', '.jpg', '.jpeg']
 export const isImage = (filename) =>{
     const ext = path.extname(filename).toLowerCase()
     return IMAGE_EXTENSIONS.includes(ext)
+}
+
+export const MAX_WIDTH = 650
+
+export const isdir = async (filename) =>{
+    const stat = await fs.stat(filename)
+    return stat.isDirectory()
 }
